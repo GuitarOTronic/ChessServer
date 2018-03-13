778ff70766084b56ecdf6ee384de6bcfe73b8cff
Sean Obringer <SeanObringer90@gmail.com>
2018-03-12 18:46:51 -0700

    housekeeping

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-var app = require('express')();
-var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-=======
 const path = require('path')
 
 const express = require('express')
