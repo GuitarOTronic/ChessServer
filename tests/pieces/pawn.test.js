1d45debf2068dd5bafb8ecfb7f6d2e74ee8dd598
Sean Obringer <SeanObringer90@gmail.com>
2018-04-17 14:23:25 -0700

    saving

--- tests/pieces/pawn.test.js
+++ tests/pieces/pawn.test.js
@@ -1,1 +1,33 @@
+let {
+  Game,
+  Player,
+  Board,
+  Piece,
+  Pawn,
+  Knight,
+  Bishop,
+  Rook,
+  Queen,
+  King,
+  History
+} = require('../../logic/game.js')
+
+
 /*PlaceHolder*/
+
+let chai = require('chai')
+let chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+let expect = chai.expect
+
+
+function startNewGame(){
+  return new Board()
+}
+
+
+let testGame = startNewGame()
+console.log(testGame.board)
+
+
+describe('pawnTest', )
